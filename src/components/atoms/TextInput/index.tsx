d44e4e77e2ccd9f6ef06ffc808930ac5942e9e35
hanchchch <cndghks15@gmail.com>
2021-08-23 01:16:34 +0900

    feat: TextInput

--- src/components/atoms/TextInput/index.tsx
+++ src/components/atoms/TextInput/index.tsx
@@ -1,20 +1,29 @@
 import React from "react";
+import styles from "./index.module.scss";
 
 
 interface TextInputProps {
+  label: string;
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
 }
 
 export default function TextInput(props: TextInputProps) {
-  const {  value, onChange, placeholder = "" } = props;
+  const {  label, value, onChange, placeholder = "" } = props;
   return (
-    <input
-      type="text"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder={placeholder}
-    />
+    <div className={styles.wrapper}>
+      <div className={styles.head}>
+        {label && `*${label}`}
+      </div>
+      <div className={styles.container}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
   );
 }
