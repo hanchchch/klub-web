8b5a29b0276aa94a8d93d1e2483ff41cda29feb6
hanchchch <cndghks15@gmail.com>
2021-08-24 01:54:52 +0900

    feat: payment product and options confirm

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -6,13 +6,20 @@ import useOrders from "@src/utils/hooks/order";
 import { Ellipse } from "@src/components/atoms/Ellipse";
 import router from "next/router";
 import HeaderLayout from "@src/components/templates/HeaderLayout";
-import { deliveryCharge } from "@src/utils/store";
+import { categoryOptions, deliveryCharge, options } from "@src/utils/store";
+import { Product } from "@src/types/product";
 
 export default function Payment() {
   const [orders] = useOrders();
   const [orderer] = useOrderer();
 
   const [price, setPrice] = useState<number>(0);
+
+  const optionKeys = (product: Product) => product.category.map(category => (
+    categoryOptions[category].map(option => (
+      options[option].label
+    ))
+  )).flat();
 
   useEffect(() => {
     setPrice(
@@ -30,19 +37,24 @@ export default function Payment() {
         <hr />
         <table>
           <tbody>
-            <tr>
-              <td>Subtotal / 합계</td>
-              <td>₩{price}</td>
-            </tr>
+            {orders.map(o => (
+              <tr key={o.product.name}>
+                <td>{o.product.name}</td>
+                <td>{optionKeys(o.product).map(k => o.options[k]).join(" / ")}</td>
+                <td>₩{o.product.price}*{o.options.quantity}</td>
+              </tr>)
+            )}
             <tr>
               <td>Shipping / 배송</td>
+              <td></td>
               <td>₩{orderer.isShipping ? deliveryCharge : 0}</td>
             </tr>
             <tr>
-              <td colSpan={2}><hr className={styles.bold}/></td>
+              <td colSpan={3}><hr className={styles.bold}/></td>
             </tr>
             <tr className={styles.total}>
               <td>TOTAL</td>
+              <td></td>
               <td>₩{price + (orderer.isShipping ? deliveryCharge : 0)}</td>
             </tr>
           </tbody>
