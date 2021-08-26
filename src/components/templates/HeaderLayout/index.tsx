c2b03cb28ca7ed507791c15c7ad25acee5f2f06b
hanchchch <cndghks15@gmail.com>
2021-08-27 00:09:53 +0900

    fix: logo img instead of embed

--- src/components/templates/HeaderLayout/index.tsx
+++ src/components/templates/HeaderLayout/index.tsx
@@ -2,20 +2,17 @@ import React from "react";
 import styles from "./index.module.scss";
 import Main from "@src/components/templates/Main";
 
-
-export default function HeaderLayout(props: { children?: React.ReactNode, className?: string }) {
+export default function HeaderLayout(props: { children?: React.ReactNode; className?: string }) {
   return (
     <Main>
       <div className={styles.header}>
         <a href="/">
-          <div className={styles.logo} >
-            <embed src={"/assets/klub_logo.svg"}/>
+          <div className={styles.logo}>
+            <img src={"/assets/klub_logo.svg"} />
           </div>
         </a>
       </div>
-      <div className={`${styles.container} ${props.className ?? ""}`}>
-        {props.children}
-      </div>
+      <div className={`${styles.container} ${props.className ?? ""}`}>{props.children}</div>
     </Main>
   );
 }
