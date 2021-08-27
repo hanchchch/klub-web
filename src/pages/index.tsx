61c47231ee8705b36986f2f259aa6531f42a355a
hanchchch <cndghks15@gmail.com>
2021-08-27 19:38:32 +0900

    fix: max-width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,19 +40,21 @@ export default function Index() {
             <ProductCard key={product.id} product={product} />
           ))}
       </div>
-      <div className={styles.notice}>
-        <div>
-          <div className={styles.title}>서버 지원</div>
-          <div className={styles.content}>
-            <a href="https://devkorops.notion.site/DevKor-4ad582d6659647edb9131625da3da387">
-              소프트웨어 개발 연구 학회 DevKor
-            </a>
+      <div className={styles.wrapper}>
+        <div className={styles.notice}>
+          <div>
+            <div className={styles.title}>서버 지원</div>
+            <div className={styles.content}>
+              <a href="https://devkorops.notion.site/DevKor-4ad582d6659647edb9131625da3da387">
+                소프트웨어 개발 연구 학회 DevKor
+              </a>
+            </div>
           </div>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.title}>문의</div>
-          <div className={styles.content}>
-            <a href="https://www.instagram.com/klub_project/">instagram @klub_project</a>
+          <div className={styles.right}>
+            <div className={styles.title}>문의</div>
+            <div className={styles.content}>
+              <a href="https://www.instagram.com/klub_project/">instagram @klub_project</a>
+            </div>
           </div>
         </div>
       </div>
